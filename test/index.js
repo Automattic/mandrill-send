fa14e615b15a9a2a3e181fa7002bbf87427e4b27
Damian Suarez <rdsuarez@gmail.com>
2013-02-12 12:21:49 -0300

    test: add first expect test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,32 @@
-var mandril = require('../');
 
-describe('send a simepl email', function(){
-  it('hould be return an success response', function(done){
-    return done();
+/**
+ * Module dependencies
+ */
+
+var mandrill = require('../')
+  , config = require('./config')
+  , expect = require('expect.js');
+
+/**
+ * Create Mandill-send instance
+ */
+
+var email = mandrill(config.key);
+
+describe('send a simple email', function(){
+  it('expect not `null` response', function(done){
+
+    email({
+      from: 'Rosepad <damian@rosepad.com>',
+      to: [
+        'rdsuarez@gmail.com',
+        { name: 'Dami', email: 'damian.suarez@xifox.net' }
+      ],
+      subject: 'Testing signup',
+      tags: ['signup', 'welcome']
+    }, function(err){
+      expect(err).to.equal(null);
+      done();
+    });
   });
 });
