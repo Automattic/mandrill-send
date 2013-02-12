34eea44dac9ae62fbe51d9456a09d21bea63ec36
Guillermo Rauch <rauchg@gmail.com>
2013-02-12 09:37:57 -0800

    test: mock based testing

--- test/index.js
+++ test/index.js
@@ -3,18 +3,48 @@
  * Module dependencies
  */
 
-var mandrill = require('../')
-  , config = require('./config')
-  , expect = require('expect.js');
+var mandrill = require('../');
+var expect = require('expect.js');
 
 /**
  * Create Mandill-send instance
  */
-var email = mandrill(config.key);
+var email = mandrill('some random key');
 
-describe('send a simple email', function(){
-  it('expect `null` error response', function(done){
+describe('mandrill-send', function(){
+  it('should work', function(done){
+    // mock
+    var req = {};
+    req.post  = function(_url){
+      req.url = _url;
+      return req;
+    };
+    req.send = function(data){
+      req.data = data;
+      return req;
+    };
+    req.end = function(fn){
+      expect(req.url).to.be('https://mandrillapp.com/api/1.0/messages/send.json');
+      expect(req.data).to.eql({
+        key: 'some random key',
+        message: {
+          from_name: 'From',
+          from_email: 'from@email.com',
+          to: [
+            { email: 'to@email.com' },
+            { name: 'The other guy', email: 'the.other.guy@email.net' }
+          ],
+          subject: 'Testing signup',
+          tags: ['signup', 'welcome']
+        }
+      });
+      done();
+    };
 
+    // set mock
+    mandrill.request = req;
+
+    // send email
     email({
       from: 'From <from@email.com>',
       to: [
@@ -23,9 +53,6 @@ describe('send a simple email', function(){
       ],
       subject: 'Testing signup',
       tags: ['signup', 'welcome']
-    }, function(err){
-      expect(err).to.equal(null);
-      done();
     });
   });
 });
